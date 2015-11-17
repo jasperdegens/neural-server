37d0ce131e75da0dfef36a5e7dd9ac4801587436
Jasper Degens <jasperdegens@gmail.com>
2015-11-17 17:03:09 +0100

    added compression, compressed images

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compress = require('compression');
 
 var apiRoutes = require('./routes/apiRoutes');
 
@@ -16,12 +17,14 @@ mongoose.connect('mongodb://localhost/test');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(compress());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Main Routes for original app
 app.get('/', function(req, res){
